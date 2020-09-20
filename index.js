7c64da2be848c201d95e2d10c63447932ae64396
rexcloud1 <68808736+rexcloud1@users.noreply.github.com>
2020-09-20 12:20:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
 require('./bot');
