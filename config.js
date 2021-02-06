e16cce134f5a2b3188ff379c99e28041b367fc7b
rexcloud1 <68808736+rexcloud1@users.noreply.github.com>
2021-02-06 13:41:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    botToken:'<927663185:AAGGsHXtkX6BEq2dq_3IEp4tGSA0gxeOsdI>',
-    bitlyToken:'<f1cf97c4ee568f28a299071f68e85c3df7f38627>'
+    botToken:'1404500286:AAFjbiok3Mof0d-VBC-edo1Ahym_PX9N8iE',
+    bitlyToken:'f1cf97c4ee568f28a299071f68e85c3df7f38627'
 };
